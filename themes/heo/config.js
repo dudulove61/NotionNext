a4034a6ad4b7d4a7c66a1c974d58bc0a9a3e9328
Tox <dudulove61@gmail.com>
2025-05-17 10:53:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,11 +17,11 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '鲜艳的红领巾',
+  HEO_HERO_TITLE_2: '飘扬在胸前',
   HEO_HERO_TITLE_3: 'TOX.WANG',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_4: 'TOX Blog',
+  HEO_HERO_TITLE_5: '干货满满',
   HEO_HERO_TITLE_LINK: 'https://tox.wang',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
